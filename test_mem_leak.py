c74feec0714bc0a5aca146cd3c2070756744c99f
Ryan Avery <ryanbarryavery@gmail.com>
2025-05-21 10:48:49 -0700

    increase bs for mem leak test, mem leak test passes with 2.7

--- test_mem_leak.py
+++ test_mem_leak.py
@@ -3,8 +3,8 @@ model_path = "test.pt2"
 model = torch._inductor.aoti_load_package(model_path)
 device = torch.device("cuda" + ":" + str(torch.cuda.current_device()))
 torch.cuda.set_device(device)
-test_arr = torch.randn((10, 9 * 4, 1024, 1024), device=device)
-for i in range(2):
+test_arr = torch.randn((16, 9 * 4, 1024, 1024), device=device)
+for i in range(10):
     model(test_arr)
     print(i)
 print("no leak")
--- test_no_channel_error.py
+++ test_no_channel_error.py
@@ -2,6 +2,7 @@ import torch
 model = torch._inductor.aoti_load_package('test.pt2')
 
 # comment this out and then the second block will error
+# I would expect this to error with both the first and second blocks
 t = torch.rand(1, 36, 1024, 1024).to('cuda')
 print("Input shape", t[:,0:9,...].shape)
 model(t[:,0:9,...])
